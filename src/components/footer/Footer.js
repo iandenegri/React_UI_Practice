69a58a08c06f92067bac66e2f248bc313d87afb6
Ian Denegri <iandenegri@hotmail.com>
2018-11-19 13:58:46 -0500

    Setting up repo.

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import './Footer.css';
+
+const Footer = () => {
+  
+  return (
+    <div className="App">
+      My Footer. :-D
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/header/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import './Header.css';
+
+class Header extends Component {
+
+
+
+  render() {
+    return (
+      <div className="App">
+        My header. :-D
+      </div>
+    );
+  }
+}
+
+export default Header;
